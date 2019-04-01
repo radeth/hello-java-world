6ad36167b98f2db869811ca88dd3f3fea9cb5102
radeth <123radeth@gmail.com>
2019-04-02 00:46:54 +0200

    initial commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,26 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        Message text = new Message();
-        System.out.println(text.printOnCmd());
+        String stones = "aAAbbbb";
+        String jewls = "aA";
+        String[] stonesParts = stones.split("");
+        String[] jewelsParts = stones.split("");
+        int counter = 0;
+        for(int i=0;i<stonesParts.length;i++){
+            if(checkRarity(stonesParts[i],jewelsParts)==true){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+    public static boolean checkRarity(String possibleJewel, String[] jewelsPart){
+        for(int i=0;i<jewelsPart.length;i++){
+            System.out.println(possibleJewel);
+            System.out.println(jewelsPart[i]);
+            if(possibleJewel==jewelsPart[i]){
+                return true;
+            }
+        }
+        return false;
     }
 }
